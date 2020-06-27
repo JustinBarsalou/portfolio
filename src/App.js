e1ad7a7d7f5f7611ba87ee2912b8ffff8d06b284
JustinBarsalou <justinbarsalou@gmail.com>
2020-06-26 19:21:29 -0500

    added name and job

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="main-app">
+        <div className = 'name-container'>
+          <h1 className = 'name'>Justin Barsalou</h1>
+        </div>
+        <div className = 'job-container'>
+          <h1 className = 'job-title'>Full Stack Web Development</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
